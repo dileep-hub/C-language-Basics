4e9f1551fca6be3c19b2a0e6f99a0566dad30008
Dileep Kumar.S.R <dileepkumar1422002@gmail.com>
2020-09-15 18:56:55 +0530

    Added basic -1

---
+++ basic.c
@@ -0,0 +1,26 @@
+#include <stdio.h>   // take input and give output
+#include <stdlib.h>  // for which helps to find functions
+
+int main()
+{
+    printf("Hello world!\n");   /// \n for new line
+    printf("%d \n", 89);
+    printf("%f \n", 45.7);
+    printf("%ld \n", 746473637);  /// large integer
+    printf("%f \n", 45.7);
+    printf("%lf \n", 454466.4);  /// large float
+    printf("%s \n", "how r u");  /// here in string only use ""
+    printf("%c \n", 'how');  /// output is only w  --> use only ''   c for char
+    return 0;
+}
+
+
+/// for multi line comments we use /**/
+/* hello
+   how
+   r
+   u
+   */
+
+   /// for single line comment we use //
+   // hello how r u
