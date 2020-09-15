ad3bbd3b57e7ad1d8f2d4a63424fa2e8c037a90b
Dileep Kumar.S.R <dileepkumar1422002@gmail.com>
2020-09-15 18:57:48 +0530

    Added basic-2

---
+++ basic 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int my_age;
+    float pi= 3.14;
+    char my_char = 'dileep';
+    double big_pi = 3.1422222222222;
+    printf("my age is %d \n", 34);
+    printf("my age is %c \n", my_char);
+    printf("my age is %lf \n", big_pi);
+    return 0;
+}
