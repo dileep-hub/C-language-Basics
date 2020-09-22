512d64f83be879aead9faccd587996a8266ec6be
Dileep Kumar.S.R <dileepkumar1422002@gmail.com>
2020-09-22 11:29:31 +0530

    Added Operators

---
+++ operator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*int a = 24, b = 12;
+    int sum;                      for sum we use int
+    sum = a + b;
+    printf("Sum = %d", sum);      for sum we wrote %d        */
+
+    /*int a = 9, b = 2;
+    float div;
+    div = a / b;
+    printf("division = %f", div);      here we got 4.00000 */
+
+    /*int a = 9, b = 2;
+    float mod;
+    mod = a % b;
+    printf("modulus = %f", mod);       here we got 1.000000 */
+
+    /* even we can use * also for multiplication */
+
+    return 0;
+
+}
